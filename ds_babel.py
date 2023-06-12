c051496994b8918e80dec3ff23509330c4c85739
Tolbaev Muntasir <mtolbaev2000@gmail.com>
2023-06-12 10:15:52 +0500

    csv_to_sql

--- ds_babel.py
+++ ds_babel.py
@@ -7,7 +7,8 @@ def sql_to_csv(database, table_name):
     con = sql.connect(database)
     df = pd.read_sql_query(f"SELECT * FROM {table_name}", con)
 
-    col_names = list(df.columns())
+    # col_names = list(df.columns())
+    col_names = df.columns.tolist()
     rows = [list(row) for row in df.values]
 
     csv_str = ','.join(col_names) + '\n'
@@ -17,3 +18,25 @@ def sql_to_csv(database, table_name):
 
     con.close()
     return csv_str
+
+##################
+##############
+##########
+
+def csv_to_sql(csv_content, data, table):
+    
+    df = pd.read_csv(csv_content)
+    con = sql.connect(data)
+
+    col_str = ','.join('"{}"'.format(col.replace('_', ' ')) for col in df.columns)
+
+    query = f"CREATE TABLE IF NOT EXISTS {table} ({col_str})"
+    con.execute(query)
+
+    for row in df.itertuples(index=False):
+        query = f"INSERT INTO {table} VALUES ({','.join('?' * len(row))})"
+        con.execute(query, row)
+
+    con.commit()
+
+    con.close()
